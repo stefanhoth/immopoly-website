f0ef60fdc095f9ba0f5051c6b15d62ec1f0edcd0
Stefan Hoth <sh@jnamic.com>
2012-02-04 21:21:11 +0100

    Add subpage for download

---
+++ content/download.inc.php
@@ -0,0 +1,52 @@
+<h1>Downloaden der App</h1>
+<p><strong>Dank der vielen durchgearbeiteten Wochenenden können wir bereits Apps für iOS (iPhone &amp; iPad) als auch für Android zum Download anbieten.</strong> Lad einfach die passende Version für Dein Gerät herunter und mach den Immobilienmarkt unsicher!</p> 
+<div class="row">
+  <div class="span6">
+    <h2>iPhone &amp; iPad</h2>
+    <h4>Offizielle, stabile Version</h4>
+    <p>
+    Dank des fleißigen iOS-Teams können wir euch bereits eine iPhone- &amp; iPad-Version direkt aus dem App Store anbieten. Gleich herunterladen! 
+    </p>
+    <p style="text-align: center;">
+    <a class="none" href="http://itunes.apple.com/de/app/immopoly/id496603059" title="Immopoly im Apple App Store für dein iPhone und iPad">
+    <img src="img\layout\app-store.png" alt="App Store" width="200" />
+    <span style="display:block;clear:both;width: 100%;">(click)</span>
+    </a> 
+    </p>
+    <h4>BETA / ALPHA version</h4>
+    <p class="alert alert-info">
+      Leider erlaubt es die Distributionspolitik von Apple nicht, dass wir iOS-Nutzern BETA- oder ALPHA-Versionen zum Download bereitstellen. Daher gibt es hier jeweils nur die Version, die sich im App Store befindet. 
+    </p>
+  </div>
+  <div class="span6">
+      <h2>Android</h2>
+      <h4>Offizielle, stabile Version</h4>
+      <p>Wir arbeiten derzeit mit Hochdruck an der Fertigstellung des Releases für den <strong>Android Market</strong>.</p>
+      <p>Bis dahin bieten wir euch zwei verschiedene Möglichkeiten am Spielgeschehen teilzunehmen:</p>
+     
+      <h4>BETA version</h4> 
+<p class="alert alert-warning"><strong>Hinweis:</strong> Diese Version ist bereits getestet, kann aber noch Fehler enthalten. Wenn Du Fehler findest, hilfst Du uns sehr, wenn Du diese meldest.</p>
+      <p style="text-align: center;">
+        <a class="none" href="http://immopoly.appspot.com/Immopoly.apk">
+          <img src="img\layout\immopoly-download-qr.png" alt="QR-code" />
+          <span style="display:block;clear:both;width: 100%;">(click)</span>
+        </a>
+      </p>  
+        
+      <h4>ALPHA version</h4> 
+      <p class="alert alert-error"><strong>Achtung!</strong> Diese Version ist top-aktuell und beinhaltet damit die neuesten Funktionen und Layouts. Da sie aber noch nicht ausreichend getestet ist, kann es zu Abstürzen oder anderen Fehlfunktionen kommen. Wenn Du Fehler findest, hilfst Du uns sehr, wenn Du diese meldest.</p>
+      <a href="http://www.immopoly.org/apk/immopoly-android-1.0.0-SNAPSHOT.apk">http://www.immopoly.org/apk/immopoly-android-1.0.0-SNAPSHOT.apk</a>
+      </p>
+
+
+  </div>
+</div>
+<div>
+  <h2>Bugs, Featurewünschen, Hinweise</h2>
+  <p>Immopoly wird als Open-Source-Projekt entwickelt. Das bedeutet, dass viele Freiwillige ihre Zeit und Ideen in das Projekt einfließen lassen und es damit stetig besser wird. Durch die Quelloffenheit, kann sich jeder mit Programmierkenntnissen leicht ein Bild von der Arbeitsweise unserer Software machen.</p>
+  <p>Wir verwalten Ideen und Fehler in einem sog. Bug-Tracker, der uns hilft, den Überblick zu erhalten. Du würdest uns sehr helfen, wenn Du Deine Fehlerberichte und Funktionswünsche dort eintragen würdest.</p>
+  <p><a class="btn btn-primary" href="https://github.com/immopoly/immopoly/issues">Zum Issue-Tracker &raquo;</a></p>
+  <p>
+    Sollte es Dir im Ausnahmefall nicht möglich sein, uns so eine Nachricht zukommen zu lassen, so kannst Du uns auch eine E-Mail schreiben: <a href="mailto:immopoly-devs@googlegroups.com?subject=Nachricht via Website" title="Mailprogramm öffnen...">immopoly-devs@googlegroups.com</a>
+  </p>
+</div>
