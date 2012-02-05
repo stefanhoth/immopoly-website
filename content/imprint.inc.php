eaacfd827ee30a6178109236b938fa66d9c78300
Stefan Hoth <sh@jnamic.com>
2012-02-05 09:32:45 +0100

    Make imprint more beautiful

--- content/imprint.inc.php
+++ content/imprint.inc.php
@@ -8,17 +8,34 @@ Verantwortlicher Ansprechpartner für diese Webseite ist:
 <h2>Credits</h2>
 <p>Wir bedanken uns bei den Künstlern, die uns Grafikresourcen gratis zur Verwendung überlassen. CC-Lizenzen rocken!</p>
 
-<h3>Titelbild</h3>
-<p>Das Titelbild der Startseite haben wir dem Flickr-User <a href="http://www.flickr.com/photos/giesenbauer/" title="Flickr-Profil">Bjørn Giesenbauer</a> zu verdanken. Hier gibt es das Bild:
-</p>
-<p><a href="http://www.flickr.com/photos/giesenbauer/3458525781/" title="Bilddownload auf Flickr" class="btn">Zur Flickr-Galerie &raquo;</a></p>
 
-<h3>Icons</h3>
+<div class="row">
+	
+	<div class="span4">
 
-<p>
-<a href="http://glyphicons.com/">Glyphicons</a> stellte Twitter für dieses Template die Icons (z.B. verwendet in der Navigation) zur Verfügung. Dafür danken wir!
-</p>
-<p><a href="http://glyphicons.com/" class="btn">Zum Glyphicons-Set &raquo;</a></p>
+		<h3>Titelbild</h3>
+		<p>Das Titelbild der Startseite haben wir dem Flickr-User <a href="http://www.flickr.com/photos/giesenbauer/" title="Flickr-Profil">Bjørn Giesenbauer</a> zu verdanken. Hier gibt es das Bild:
+		</p>
+		<p><a href="http://www.flickr.com/photos/giesenbauer/3458525781/" title="Bilddownload auf Flickr" class="btn">Zur Flickr-Galerie &raquo;</a></p>
 
-<p>Auf der <a href="description.html">Beschreibungsseite</a> verwendeten wir Icons aus dem <strong>Milky Icon Set</strong>. Auch für diese kostenfreie Nutzungsmöglichkeit bedanken wir uns.
-<p><a href="http://www.iconeden.com/icon/milky-a-free-vector-iconset.html" class="btn">Zum Milky-Icon-Set &raquo;</a></p>
+	</div>
+	<div class="span4">
+		<h3>Bootstrap</h3>
+
+		<p>Diese Website ist auf Basis des Frameworks Bootstrap 2.0 entstanden, welches von Twitter erstellt und veröffentlicht wurde.
+
+		<p><a href="http://twitter.github.com/bootstrap/index.html" title="Zur Bootstrap-Projektseite" class="btn">Zur Bootstrap-Projektseite &raquo;</a></p>
+	</div>
+	<div class="span4">
+		<h3>Icons</h3>
+
+		<p>
+		<a href="http://glyphicons.com/">Glyphicons</a> stellte Twitter für dieses Template die Icons (z.B. verwendet in der Navigation) zur Verfügung. Dafür danken wir!
+		</p>
+		<p><a href="http://glyphicons.com/" class="btn">Zum Glyphicons-Set &raquo;</a></p>
+
+		<p>Auf der <a href="description.html">Beschreibungsseite</a> verwendeten wir Icons aus dem <strong>Milky Icon Set</strong>. Auch für diese kostenfreie Nutzungsmöglichkeit bedanken wir uns.
+		<p><a href="http://www.iconeden.com/icon/milky-a-free-vector-iconset.html" class="btn">Zum Milky-Icon-Set &raquo;</a></p>
+	</div>
+</div>
+
