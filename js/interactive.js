f6b77454f3137c82bba140aaf5ef71d15f1f2adc
Stefan Hoth <sh@jnamic.com>
2012-02-05 19:03:35 +0100

    Showing relative time in history now

--- js/interactive.js
+++ js/interactive.js
@@ -129,8 +129,7 @@
 		case "history":
 			history = jsonData["org.immopoly.common.History"];
 			
-			date = new Date(history.time);
-			dateString =  date.getDate() + "." + pad((date.getMonth() + 1),2) + "." + date.getFullYear() + "&nbsp;" + date.getHours() + ":" + pad(date.getMinutes(),2);
+			dateString = new Date(history.time).toRelativeTime();
 			
 			logger(history);
 			logger(history.username);
@@ -252,3 +251,80 @@
       } 
     ); 
     
+
+
+
+/**
+ * Returns a description of this past date in relative terms.
+ * Takes an optional parameter (default: 0) setting the threshold in ms which
+ * is considered "Just now".
+ *
+ * Examples, where new Date().toString() == "Mon Nov 23 2009 17:36:51 GMT-0500 (EST)":
+ *
+ * new Date().toRelativeTime()
+ * --> 'Just now'
+ *
+ * new Date("Nov 21, 2009").toRelativeTime()
+ * --> '2 days ago'
+ *
+ * // One second ago
+ * new Date("Nov 23 2009 17:36:50 GMT-0500 (EST)").toRelativeTime()
+ * --> '1 second ago'
+ *
+ * // One second ago, now setting a now_threshold to 5 seconds
+ * new Date("Nov 23 2009 17:36:50 GMT-0500 (EST)").toRelativeTime(5000)
+ * --> 'Just now'
+ *
+ */
+Date.prototype.toRelativeTime = function(now_threshold) {
+  var delta = new Date() - this;
+
+  now_threshold = parseInt(now_threshold, 10);
+
+  if (isNaN(now_threshold)) {
+    now_threshold = 0;
+  }
+
+  if (delta <= now_threshold) {
+    return 'Gerade jetzt';
+  }
+
+  var units = null;
+  var conversions = {
+    Millisekunde: 1, // ms    -> ms
+    Sekunde: 1000,   // ms    -> sec
+    Minute: 60,     // sec   -> min
+    Stunde:   60,     // min   -> hour
+    Tag:    24,     // hour  -> day
+    Monat:  30,     // day   -> month (roughly)
+    Jahr:   12      // month -> year
+  };
+
+  for (var key in conversions) {
+    if (delta < conversions[key]) {
+      break;
+    } else {
+      units = key; // keeps track of the selected key over the iteration
+      delta = delta / conversions[key];
+    }
+  }
+
+  // pluralize a unit when the difference is greater than 1.
+  delta = Math.floor(delta);
+  if (delta !== 1) { 
+    if(units.charAt(units.length-1) == "e"){
+      units += "n";  
+    }else{
+      units += "en";
+    }
+  }
+  return ["vor",delta, units].join(" ");
+};
+
+/*
+ * Wraps up a common pattern used with this plugin whereby you take a String
+ * representation of a Date, and want back a date object.
+ */
+Date.fromString = function(str) {
+  return new Date(Date.parse(str));
+};
