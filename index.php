a9367f1bbafea6bfcb106793fb9037bf9449f131
Stefan Hoth <sh@jnamic.com>
2012-02-05 11:11:50 +0100

    Repair mobile nav

--- index.php
+++ index.php
@@ -55,6 +55,11 @@
     <div class="navbar navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
+           <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
           <a class="brand" href="/">Immopoly</a>
           <div class="nav-collapse">
             <ul class="nav">
